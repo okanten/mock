b732d42be242ee671a68c4a59f83bc6a8a86003a
O.K <31588139+okstudies@users.noreply.github.com>
2018-07-04 10:08:00 +0200

    Add files via upload

---
+++ mock.py
@@ -0,0 +1,14 @@
+import sys
+
+def main(args):
+    input = list(args.lower())
+    print(input)
+    result = ""
+    for i, letter in enumerate(input):
+        if i % 2 == 0:
+            result = result + letter.upper()
+        else:
+            result = result + letter
+    print (result)
+
+main(sys.argv[1])
