4a75a7c2ba19e9bae2c9805663d125a7268143bd
Ola Kanten <31588139+okstudies@users.noreply.github.com>
2018-07-19 01:05:39 +0200

    Added clipboard functionality

--- mock.py
+++ mock.py
@@ -1,13 +1,16 @@
-import sys
+import sys, pyperclip
 
-def main(args):
-    input = list(args.lower())
-    result = ""
-    for i, letter in enumerate(input):
-        if i % 2 == 0:
-            result = result + letter.upper()
-        else:
-            result = result + letter
-    print (result)
-
-main(sys.argv[1])
+if len(sys.argv) > 1:
+    input = list(sys.argv[1].lower())
+else:
+    input = list(pyperclip.paste())
+result = ""
+for i, letter in enumerate(input):
+    if i % 2 == 0:
+        result = result + letter.upper()
+    else:
+        result = result + letter
+if list(pyperclip.paste()) == input:
+    pyperclip.copy(result)
+else:
+    print(result)
